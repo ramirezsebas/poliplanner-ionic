0cdf46fdba15ec04eb3a2ef02b8ff739b0fc0c95
fer334 <luis.fer334.fa@gmail.com>
2021-01-15 23:54:29 -0300

    DELETE SRC(copia)

--- src (copia)/app/componentes/ion2-calendar/services/calendar-options.provider.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-import { CalendarModalOptions } from '../calendar.model';
-
-export const DEFAULT_CALENDAR_OPTIONS = new InjectionToken<CalendarModalOptions>('DEFAULT_CALENDAR_MODAL_OPTIONS');
