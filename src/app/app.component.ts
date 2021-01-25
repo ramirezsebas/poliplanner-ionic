f0282d877ee6a9a09e07207b13958d3c39825d2e
fer334 <luis.fer334.fa@gmail.com>
2021-01-25 18:42:18 -0300

    auto update added(no tested)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,29 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public updates:SwUpdate
   ) {
     this.initializeApp();
+    updates.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+    });
+    updates.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => this.updateApp());
+    });
   }
 
+  updateApp(){
+    document.location.reload();
+    console.log("The app is updating right now");
+
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
